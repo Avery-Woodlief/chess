781e9cfa22276cefd899f061363d34f7ffbe5a11
Avery <averywoodlief@gmail.com>
2026-08-21 14:11:06 -0500

    update

--- src/parser/regex_maker.py
+++ src/parser/regex_maker.py
@@ -9,13 +9,10 @@ value_bind = r'''
 \s*,\s*
 (?:BIND\s*:\s*)?
 (?P<bind>\w+)
-\s*,\s*
-(?:CONTEXT\s*:\s*)?
-(?P<context>\w+)
 \]'''
 
 def compile_condition(flags=re.VERBOSE) -> re.Pattern[str]:
-    return re.compile(r"(?<=\().*(?=\))", flags=flags)
+    return re.compile(r"\([^\)]+\)", flags=flags)
 
 def compile_binary_operation(operation="AND", flags=re.VERBOSE) -> re.Pattern[str]:
     return re.compile(rf'''(?P<LHS>\(.*\))\s*{operation.upper()}\s*(?P<RHS>\(.*\))''', flags=flags)
@@ -26,8 +23,8 @@ def compile_unary_operation(operation="NOT", flags=re.VERBOSE) -> re.Pattern[str
 
 if __name__ == "__main__":
     condition = compile_condition()
-    QUERY = "(MOVES_MADE[VALUE:0, BIND:moves_made, CONTEXT:self]) AND (EMPTY AT NORTH[VALUE:1..2, BIND:distance, CONTEXT:global])"
-    conditions = condition.split(QUERY)
+    QUERY = "(MOVES_MADE[VALUE:0, BIND:moves_made]) AND (EMPTY AT NORTH[VALUE:1..2, BIND:distance])"
+    conditions = condition.findall(QUERY)
     print(conditions)
     AND = compile_binary_operation("and")
     print(AND.search(QUERY).groupdict())
